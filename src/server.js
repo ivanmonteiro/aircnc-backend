7f84e5785004f1a0c555906e58c92be0496c0f7e
Default Random <Ivan Monteiro Cantalice>
2019-10-04 19:03:52 -0300

    CORS fix

--- src/server.js
+++ src/server.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 
 const app = express();
 
-
 let dbdata = fs.readFileSync(path.resolve(__dirname, 'config', 'dbconfig.json'));
 let dbconfig = JSON.parse(dbdata);
-console.log(dbconfig);
 
 mongoose.connect(dbconfig.uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
